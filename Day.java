762a5a9f0a9a66140ccf79642261d03968aa4430
Saurabh-108 <saurabh775991@gmail.com>
2026-09-16 10:51:01 +0530

    Add Java practice programs

---
+++ Day.java
@@ -0,0 +1,27 @@
+public class Day {
+    public static void main(String[]args){
+
+int a = 10; 
+int b = 20;
+
+// Addition, Subtraction, Multiplication, Division, Modulus
+
+        System.out.println("Add " + (a + b));
+         System.out.println("Subtract " + (a - b));
+         System.out.println("Multiply " + (a * b));
+         System.out.println("Divide " + (a / b));
+         System.out.println("Modulus " + (a % b));
+
+// Relational Operators
+
+         System.out.println("Equal " + (a == b));
+         System.out.println("Not Equal " + (a != b));
+         System.out.println("Greater than " + (a > b));
+         System.out.println("Less than " + (a < b));
+         System.out.println("Greater than or equal to " + (a >= b));
+         System.out.println("Less than or equal to " + (a <= b));
+
+//         
+
+    }
+}
